a52d78a3804b2beca2f3b16e2fa00073cf917717
dbuenaño <deibycesar10@gmail.com>
2026-01-13 17:38:31 +0100

    prueba

---
+++ src/data/projects.data.ts
@@ -0,0 +1,13 @@
+import { Project } from "../types/project";
+
+export const projects = [
+  {
+    id: 1,
+    title: "Proyecto 1",
+    description: "Mi primer proyecto usando React y TypeScript.",
+    image: "",
+    liveUrl: "https://tuproyecto.vercel.app",
+    repoUrl: "https://github.com/tuusuario/proyecto1",
+    techs: ["React", "TypeScript", "Vite"],
+  },
+];
